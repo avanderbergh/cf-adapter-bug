8736a50afc18c212af4b81b82981afd9225ee94d
avanderbergh <avanderbergh@gmail.com>
2024-12-03 17:37:36 +0000

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import cloudflare from '@astrojs/cloudflare';
+import solidJs from '@astrojs/solid-js';
+
+// https://astro.build/config
+export default defineConfig({
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    }
+  }),
+  integrations: [solidJs()]
+});
